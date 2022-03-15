ff16eef1d9e71a8aedfe1def7bd3055ae8527f0b
nvadde <nvadde@uworld.in>
2022-03-15 07:51:50 +0530

    on tap animating view

--- basicAnimation/ViewController.swift
+++ basicAnimation/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
     let basicView = UIView()
     
     var yAnchor: NSLayoutConstraint!
-    var xAnchor: NSLayoutConstraint!
     var heightAnchor: NSLayoutConstraint!
     var widthAnchor: NSLayoutConstraint!
+    
+    var tapped = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +29,7 @@ class ViewController: UIViewController {
         yAnchor = basicView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         yAnchor.isActive = true
         
-        xAnchor = basicView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        xAnchor.isActive = true
+        basicView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
         heightAnchor = basicView.heightAnchor.constraint(equalToConstant: 200)
         heightAnchor.isActive = true
@@ -38,6 +38,8 @@ class ViewController: UIViewController {
         widthAnchor.isActive = true
         
         perform(#selector(animateBox), with: nil, afterDelay: 1)
+        
+        basicView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(animateOnTap)))
     }
 
     @objc fileprivate func animateBox() {
@@ -53,6 +55,34 @@ class ViewController: UIViewController {
         widthAnchor.isActive = false
         widthAnchor = basicView.widthAnchor.constraint(equalToConstant: view.frame.width - 50)
         widthAnchor.isActive = true
+        
+        UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 1.5, options: .curveEaseIn) {
+            self.view.layoutSubviews()
+        } completion: { _ in
+            
+        }
+    }
+    
+    @objc fileprivate func animateOnTap() {
+        if tapped {
+            widthAnchor.isActive = false
+            widthAnchor = basicView.widthAnchor.constraint(equalToConstant: view.frame.width/2)
+            widthAnchor.isActive = true
+        } else {
+            yAnchor.isActive = false
+            yAnchor = basicView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 200)
+            yAnchor.isActive = true
+            
+            heightAnchor.isActive = false
+            heightAnchor = basicView.heightAnchor.constraint(equalToConstant: 200)
+            heightAnchor.isActive = true
+            
+            widthAnchor.isActive = false
+            widthAnchor = basicView.widthAnchor.constraint(equalToConstant: view.frame.width - 20)
+            widthAnchor.isActive = true
+        }
+        
+        tapped = !tapped
         
         UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 1.5, options: .curveEaseIn) {
             self.view.layoutSubviews()
